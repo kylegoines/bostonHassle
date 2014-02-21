a2a3c12a34c2e898611d185a11c6f0c9acf8a6e7
kyle goines <thirteenotherslikeme@gmail.com>
2014-02-21 13:24:56 -0500

    small edits to events page

--- visual/sidebar-static.php
+++ visual/sidebar-static.php
@@ -1,8 +1,8 @@
 	<div id="chosen-shows">	
 
 		<div class="boxStatic">
-			<a href="http://www.bostonhassle.com/events-new/">
-				<h1>Click to see our full event list</h1>
+			<a href="http://www.bostonhassle.com/chosen-events/">
+				<h1>Chosen Shows</h1>
 			</a>
 			<a href="http://patron21.com/users/bostonhassle">
 				<img style="width:100%;" src="http://www.bostonhassle.com/images/2014/01/Patron21_Color.jpg">
